b9c3e2ce3d0ab6e7bcb3af08781ffd86d7eb6dc4
stellaw1 <stella01wang@gmail.com>
2020-11-14 21:49:30 -0800

    add image to firestore function

--- max-productivity/src/features/WebcamCapture.js
+++ max-productivity/src/features/WebcamCapture.js
@@ -5,61 +5,73 @@ import base64 from 'react-native-base64';
 
 export default class WebcamCapture extends Component {
 
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { imageSource: null };
-    };
+    this.state = { imageSource: null };
+  };
 
-    render() {
-        const videoConstraints = {
-            width: 1280,
-            height: 720,
-            facingMode: "user"
-        }
+  async handleUploadSuccess (filename) {
+    let downloadURL = await firebase.storage().ref('images').child(filename).getDownloadURL();
+    console.log('downloadURL', downloadURL)
 
-        const WebcamCapture = () => {
-            const webcamRef = React.useRef(null);
-
-            const capture = React.useCallback(
-              () => {
-                const imageSrc = webcamRef.current.getScreenshot();
-                console.log(imageSrc);
-
-                var substrImageSrc = imageSrc.substr(23);
-                console.log(substrImageSrc);
-
-                this.setState({ imageSource: imageSrc});
-
-                var storageRef = firebase.storage().ref('images');
-                var imageRef = storageRef.child('test.jpg');
-                imageRef.putString(base64.encode(substrImageSrc), 'base64').then(function(snapshot) {
-                    console.log('Uploaded a base64 string!');
-                });;
-              },
-              [webcamRef]
-            );
-
-            return (
-              <>
-                <Webcam
-                  audio={false}
-                  height={720}
-                  ref={webcamRef}
-                  screenshotFormat="image/jpeg"
-                  width={1280}
-                  videoConstraints={videoConstraints}
-                />
-                <button onClick={capture}>Capture photo</button>
-                {this.state.imageSource && (<img src={this.state.imageSource}/>)}
-              </>
-            );
-        }
-
-        return (
-            <div>
-                <WebcamCapture></WebcamCapture>
-            </div>
-        );
+    let newPhoto = {
+        src: downloadURL
     }
+    console.log('newPhoto', newPhoto);
+
+    await firebase.firestore().collection('photos').add(newPhoto);
+  }
+
+  render() {
+      const videoConstraints = {
+          width: 1280,
+          height: 720,
+          facingMode: "user"
+      }
+
+      const WebcamCapture = () => {
+          const webcamRef = React.useRef(null);
+
+          const capture = React.useCallback(
+            () => {
+              const imageSrc = webcamRef.current.getScreenshot();
+              console.log(imageSrc);
+
+              var substrImageSrc = imageSrc.substr(23);
+              console.log(substrImageSrc);
+
+              this.setState({ imageSource: imageSrc});
+
+              var storageRef = firebase.storage().ref('images');
+              var imageRef = storageRef.child('test.jpg');
+              imageRef.putString(substrImageSrc, 'base64', { contentType: 'image/jpg' });
+
+              this.handleUploadSuccess('test.jpg');
+            },
+            [webcamRef]
+          );
+
+          return (
+            <>
+              <Webcam
+                audio={false}
+                height={720}
+                ref={webcamRef}
+                screenshotFormat="image/jpeg"
+                width={1280}
+                videoConstraints={videoConstraints}
+              />
+              <button onClick={capture}>Capture photo</button>
+              {this.state.imageSource && (<img src={this.state.imageSource}/>)}
+            </>
+          );
+      }
+
+      return (
+          <div>
+              <WebcamCapture></WebcamCapture>
+          </div>
+      );
+  }
 }
